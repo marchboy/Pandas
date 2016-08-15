34cd44fe675b31784c478183526b7c764063945c
marchboy <jjgdut@163.com>
2016-08-16 00:27:53 +0800

    Update pandas_concatnate.py

--- pandas/pandas_concatnate.py
+++ pandas/pandas_concatnate.py
@@ -18,4 +18,17 @@ res = pd.concat([df1, df2], axis=1, join='outer')
 res = pd.concat([df1, df2], axis=1, join='inner')
 
 
+# join_axes
+res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
 
+# append
+df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
+res = df1.append(df2, ignore_index=True)
+res = df1.append([df2, df3])
+
+s1 = pd.Series([1,2,3,4], index=['a','b','c','d'])
+res = df1.append(s1, ignore_index=True)
+
+print(res)
